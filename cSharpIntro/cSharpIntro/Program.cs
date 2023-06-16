258b9d7443c3040e346280d5eb0e70f8425a50ae
AdrianBaron12 <sebastian.baron29@outlook.com>
2023-06-16 13:14:48 +0300

    HW

---
+++ cSharpIntro/cSharpIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace cSharpIntro
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello Guys 😁 !");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+           string input = Console.ReadLine();
+           Console.WriteLine(input);
+
+
+        }
+    }
+}
